5ce2d53a220b1f30f81f31b10b560c29c8546966
liamhtml <liampreisser5@gmail.com>
2020-12-03 22:15:34 -0500

    added the beginnings of popup.js script

--- src/background.js
+++ src/background.js
@@ -3,13 +3,5 @@ chrome.runtime.onInstalled.addListener(function() {
     console.log("DOGIFY INSTALLATION COMPLETE. DOGIFICATION SHALL NOW COMMENCE.");
   });
 
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-      chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'developer.chrome.com'},
-        })
-        ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-      }]);
-    });
+   new chrome.declarativeContent.ShowPageAction()
 });
--- src/popup.js
+++ src/popup.js
@@ -1,1 +1,23 @@
-dogify
+/*
+    2020
+    This is the script I use to check if the extension options are checked.
+    I do this by checking if the checkboxes are checked. Based on whether
+    they are or not, another, different script is executed which either
+    dogifies or de-dogifies the page.
+*/
+
+//  Start Option #1: Replace instances of "cat" with "dog"
+
+function toggleOption1() {
+
+  var option1 = document.getElementById("option1");
+
+  // if option1 is checked, dogify
+  if (option1.checked === true) {
+    option1.style.margin = "40px";
+  // otherwise, de-dogify
+  } else {
+    option1.style.margin = "20px";
+    }
+}
+// End Option 1
