ab6192c933e996c0b5c1e770aa5dcd7be5054245
liamhtml <liampreisser5@gmail.com>
2020-12-04 15:54:23 -0500

    Updated so that dogify.js runs when page loads

--- src/popup.js
+++ src/popup.js
@@ -8,8 +8,11 @@
 
 //  Start Option #1: Replace instances of "cat" with "dog"
 
+// checks if DOM is loaded first
+window.addEventListener('DOMContentLoaded', (event) => {
 // inline js isn't allowed by chrome, so I have to do this
 document.getElementById("option1").addEventListener("click", toggleOption1);
+});
 
 function toggleOption1() {
 
@@ -17,14 +20,18 @@ function toggleOption1() {
 
   // if option1 is checked, dogify
   if (option1.checked === true) {
+    console.log("DOGIFICATION HAS BEGUN.");
     chrome.tabs.executeScript({
           file: 'dogify.js'
         });
   // otherwise, deDogify
   } else {
+    console.log("DOGIFICATION HAS BEEN DISABLED. DE-DOGIFYING ALL CATS");
     chrome.tabs.executeScript({
           file: 'deDogify.js'
         });
     }
 }
+
+toggleOption1();
 // End Option 1
