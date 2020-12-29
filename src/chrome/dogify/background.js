fc2e7ca9d101bba8ef93a96d6f998f26c90e45bf
liamhtml <liampreisser5@gmail.com>
2020-12-29 11:32:01 -0500

    wait, i didn't even need background.js?
    *facepalms*

--- src/chrome/dogify/background.js
+++
@@ -1,8 +0,0 @@
-
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({option1: '1'}, function() {
-    console.log("DOGIFY INSTALLATION COMPLETE. DOGIFICATION SHALL NOW COMMENCE.");
-  });
-
-     new chrome.declarativeContent.ShowPageAction()
-  });
